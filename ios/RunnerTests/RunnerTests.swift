a2e622ba0bbe6ab8916df6046a97c4d2c784abae
Shi Jin <jinzishuai@gmail.com>
2026-03-29 09:59:33 -0600

    test(ios): make scene delegate assertion use host bundle

--- ios/RunnerTests/RunnerTests.swift
+++ ios/RunnerTests/RunnerTests.swift
@@ -4,19 +4,19 @@ import XCTest
 
 class RunnerTests: XCTestCase {
 
-  func testInfoPlistDeclaresFlutterSceneDelegate() throws {
-    let infoPlistPath = Bundle(for: type(of: self))
-      .bundlePath
-      .replacingOccurrences(of: "/RunnerTests.xctest", with: "/Runner.app/Info.plist")
-    let plist = NSDictionary(contentsOfFile: infoPlistPath)
+  func testInfoPlistDeclaresFlutterSceneDelegate() {
+    let plist = Bundle.main.infoDictionary
 
     let sceneManifest = plist?["UIApplicationSceneManifest"] as? [String: Any]
     let sceneConfigurations = sceneManifest?["UISceneConfigurations"] as? [String: Any]
-    let applicationScenes = sceneConfigurations?["UIWindowSceneSessionRoleApplication"] as? [[String: Any]]
+    let applicationScenes =
+      sceneConfigurations?["UIWindowSceneSessionRoleApplication"] as? [[String: Any]]
     let firstScene = applicationScenes?.first
     let delegateClassName = firstScene?["UISceneDelegateClassName"] as? String
+    let productModuleName = plist?["CFBundleExecutable"] as? String
 
-    XCTAssertEqual(delegateClassName, #"$(PRODUCT_MODULE_NAME).SceneDelegate"#)
+    XCTAssertNotNil(productModuleName, "CFBundleExecutable must be set in Info.plist")
+    XCTAssertEqual(delegateClassName, "\(productModuleName!).SceneDelegate")
   }
 
 }
